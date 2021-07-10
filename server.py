d845f7643cffe23e4d3f55187f987eff9983c8de
Aleksandr Semenov <semenlinux@gmail.com>
2021-07-10 13:31:13 +0300

    Add killing ZIP

--- server.py
+++ server.py
@@ -1,14 +1,38 @@
 import asyncio
-import datetime
+import logging
+from os import path
 
 import aiofiles
 from aiohttp import web
 
+
 INTERVAL_SECS = 1
 
 
+async def kill_all_proccesses(pid):
+    procces = await asyncio.create_subprocess_shell(
+        f"pgrep -P { pid }",
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+    )
+    stdout, stderr = await procces.communicate()
+    subprocces_pid = stdout.decode()
+    if subprocces_pid:
+        await asyncio.create_subprocess_shell(f"kill -9 { subprocces_pid }", stdout=None, stderr=None)
+        return
+    await asyncio.create_subprocess_shell(f"kill -9 { pid }", stdout=None, stderr=None)
+
+
 async def archivate(request):
+    directoryname = "test_photos"
     archive_hash = request.match_info.get("archive_hash")
+
+    if directoryname in [".", ".."]:
+        raise web.HTTPNotFound(text="Неверно указана директория с файлами на сервере")
+
+    if not path.exists(f"./{ directoryname }/{ archive_hash }"):
+        raise web.HTTPNotFound(text="Архив не существует или был удален")
+
     response = web.StreamResponse()
     response.headers["Content-Type"] = "text/html"
     response.headers["Content-Disposition"] = "inline"
@@ -18,15 +42,30 @@ async def archivate(request):
 
     proccess = await asyncio.create_subprocess_shell(
         f"zip -r - { archive_hash }",
-        cwd=r"./test_photos",
+        cwd=rf"./{ directoryname }",
         stdout=asyncio.subprocess.PIPE,
         stderr=asyncio.subprocess.PIPE,
     )
-    while True:
-        if proccess.stdout.at_eof():
-            break
-        file_data = await proccess.stdout.read(n=512000)
-        await response.write(file_data)
+    try:
+        while True:
+            if proccess.stdout.at_eof():
+                break
+            file_data = await proccess.stdout.read(n=512000)
+            logging.warning("Sending archive chunk ...")
+
+            await response.write(file_data)
+            await asyncio.sleep(3)
+
+    except asyncio.CancelledError:
+        logging.warning("Stopping zip ...")
+        await kill_all_proccesses(proccess.pid)
+        raise
+
+    finally:
+        await kill_all_proccesses(proccess.pid)
+        logging.warning("Stopping zip ...")
+        logging.warning("Download was interrupted")
+
     return response
 
 
@@ -44,4 +83,4 @@ if __name__ == "__main__":
             web.get("/archive/{archive_hash}/", archivate),
         ]
     )
-    web.run_app(app)
+    web.run_app(app, port="80")
